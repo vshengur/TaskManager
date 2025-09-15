196ed42ceb88c54f59851f122092c9e8eb08ecfb
Vyacheslav Shengur <slavcho21@gmail.com>
2025-09-15 11:25:15 +0300

    Fixed tests

--- tests/TaskManager.Tests/BusinessRulesTests.cs
+++ tests/TaskManager.Tests/BusinessRulesTests.cs
@@ -1,53 +1,49 @@
-using TaskManager.Domain;
-using TaskManager.Application.Commands;
-using TaskManager.Application;
+using Microsoft.EntityFrameworkCore;
+
+using System;
 using System.Threading;
 using System.Threading.Tasks;
+
+using TaskManager.Application.Commands;
+using TaskManager.Domain;
+using TaskManager.Infrastructure.Persistence;
+
 using Xunit;
-using Moq;
-using Microsoft.EntityFrameworkCore;
-using System.Linq;
-using System.Collections.Generic;
 
 namespace TaskManager.Tests;
 
 public class BusinessRulesTests
 {
-    private class InMemoryDb : DbContext, Application.IApplicationDbContext
+    private static AppDbContext CreateDb()
     {
-        public InMemoryDb(DbContextOptions options) : base(options) { }
-        public DbSet<TaskItem> Tasks => Set<TaskItem>();
-        public DbSet<TaskRelation> TaskRelations => Set<TaskRelation>();
-    }
-
-    private static InMemoryDb CreateDb()
-    {
-        var opts = new DbContextOptionsBuilder<InMemoryDb>()
-            .UseInMemoryDatabase(databaseName: System.Guid.NewGuid().ToString())
+        var opts = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
             .Options;
-        return new InMemoryDb(opts);
+        return new AppDbContext(opts);
     }
 
     [Fact]
     public async Task Cannot_Relate_Task_To_Itself()
     {
         using var db = CreateDb();
+
         var task = new TaskItem{ Title="A", Author="u"};
         db.Tasks.Add(task); await db.SaveChangesAsync();
 
         var handler = new AddRelationHandler(db);
-        await Assert.ThrowsAsync<System.InvalidOperationException>(() => handler.Handle(new AddRelationCommand(task.Id, task.Id), CancellationToken.None));
+        await Assert.ThrowsAsync<InvalidOperationException>(() => handler.Handle(new AddRelationCommand(task.Id, task.Id), CancellationToken.None));
     }
 
     [Fact]
     public async Task Delete_Task_With_Subtasks_Fails()
     {
         using var db = CreateDb();
+
         var parent = new TaskItem{ Title="P", Author="u"};
         var child = new TaskItem{ Title="C", Author="u", ParentTask = parent };
         db.Tasks.AddRange(parent, child); await db.SaveChangesAsync();
 
         var handler = new DeleteTaskHandler(db);
-        await Assert.ThrowsAsync<System.InvalidOperationException>(() => handler.Handle(new DeleteTaskCommand(parent.Id), CancellationToken.None));
+        await Assert.ThrowsAsync<InvalidOperationException>(() => handler.Handle(new DeleteTaskCommand(parent.Id), CancellationToken.None));
     }
 }
